d2c1f51bf7eda16fddb1fb5c02cb90afc6baa1dc
AsaHoCharlesHo <asa_ho@foxmail.com>
2019-02-11 14:00:08 +0800

    autoload-dev

--- tests/client/DadaRequestClientTest.php
+++ tests/client/DadaRequestClientTest.php
@@ -12,23 +12,54 @@
 namespace AsaHoCharlesHo\Dada\Tests\Client;
 
 use AsaHoCharlesHo\Dada\Config\Config;
-use AsaHoCharlesHo\Dada\Api\AddMerchantApi;
+use AsaHoCharlesHo\Dada\Api\BaseApi;
 use AsaHoCharlesHo\Dada\Client\DadaRequestClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class DadaRequestClientTest extends TestCase
 {
-    /*public function testBuildRequestParams()
+    public function testBuildRequestParams()
     {
         $config = new Config('10086', true, 'mock-app-key', 'mock-app-secret');
-        $param = [
-            'mock-param' => 'mock-param-value',
-        ];
-        $api = new AddMerchantApi($param);
+        $api = new DummyApi();
+        $dada_client = new DadaRequestClient($config, $api);
+        $requestParams = $dada_client->buildRequestParams();
+        $this->assertSame('mock-app-key', $requestParams['app_key']);
+        $this->assertSame(json_encode(['mock-param' => 'mock-param-value'], JSON_UNESCAPED_UNICODE), $requestParams['body']);
+    }
+
+    public function testGetHttpRequestWithPost()
+    {
+        /*$config = new Config('10086', true, 'mock-app-key', 'mock-app-secret');
+        $api = new DummyApi();
+        $url = $config->getHost().$api->getUrl();
+
         $dada_client = new DadaRequestClient($config, $api);
         $requestParams = $dada_client->buildRequestParams();
 
-        $this->assertSame('mock-app-key', $requestParams['app_key']);
-        $this->assertSame(json_encode($param, JSON_UNESCAPED_UNICODE), $requestParams['body']);
-    }*/
+        // 创建模拟接口响应值。
+        $response = new Response(200, [], '{"success": true}');
+
+        // 创建模拟 http client。
+        $client = \Mockery::mock(DadaRequestClient::class, ['10086', true, 'mock-app-key', 'mock-app-secret'])->makePartial();
+
+        // 指定将会产生的行为（在后续的测试中将会按下面的参数来调用）。
+        $client->allows()->getHttpRequestWithPost($requestParams)->andReturn($response);
+
+        // 然后调用 `getWeather` 方法，并断言返回值为模拟的返回值。
+        $this->assertSame(['success' => true], $client->getHttpRequestWithPost($requestParams));*/
+    }
+}
+
+class DummyApi extends BaseApi
+{
+    public function __construct()
+    {
+        $param = [
+            'mock-param' => 'mock-param-value',
+        ];
+        parent::__construct('/mock_url', $param);
+    }
 }
