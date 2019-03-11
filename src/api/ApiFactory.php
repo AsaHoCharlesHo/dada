bedd79203f1fda03e1595f5c4ab8b56021ebe60b
asa-charles-ho <asa_ho@foxmail.com>
2019-03-11 15:29:41 +0000

    Apply fixes from StyleCI

--- src/api/ApiFactory.php
+++ src/api/ApiFactory.php
@@ -1,13 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Asa Ho
- * Date: 2019/3/10
- * Time: 23:02
+
+/*
+ * This file is part of the asa-charles-ho/dada
+ *
+ * (c) asa ho <asa_ho@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace AsaHoCharlesHo\Dada\Api;
-
 
 class ApiFactory
 {
