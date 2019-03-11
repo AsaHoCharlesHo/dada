7612593e6175fecc629bc781113030d90ecd73b1
asa-charles-ho <asa_ho@foxmail.com>
2019-03-11 15:42:18 +0000

    Apply fixes from StyleCI

--- src/api/ApiInterface.php
+++ src/api/ApiInterface.php
@@ -1,13 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Asa Ho
- * Date: 2019/3/10
- * Time: 23:28
+
+/*
+ * This file is part of the asa-charles-ho/dada
+ *
+ * (c) asa ho <asa_ho@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
+
 namespace AsaHoCharlesHo\Dada\Api;
 
-Interface ApiInterface
+interface ApiInterface
 {
     public function getUrl();
 
